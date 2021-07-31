d5eda398a090e60203a6b9c9a4a78c1f33bcd51b
NostroGuardian <shmelivan5@yandex.ru>
2021-07-31 18:36:32 +0300

    Initial commit

---
+++ app/src/main/java/com/nostrodev/nostropanel/Dashboadr.java
@@ -0,0 +1,14 @@
+package com.nostrodev.nostropanel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dashboadr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboadr);
+    }
+}
