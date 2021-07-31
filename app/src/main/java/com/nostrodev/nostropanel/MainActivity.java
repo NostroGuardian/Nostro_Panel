247400be117c110a42eef7654b310672d574577b
NostroGuardian <shmelivan5@yandex.ru>
2021-07-31 21:28:59 +0300

    Development

--- app/src/main/java/com/nostrodev/nostropanel/MainActivity.java
+++ app/src/main/java/com/nostrodev/nostropanel/MainActivity.java
@@ -1,14 +1,86 @@
 package com.nostrodev.nostropanel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.AsyncTaskLoader;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etLogin, etPassword;
+    Button btnLogin;
+
+    final String urlLogin = "https://nstrdv.ml/api.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etLogin = (EditText) findViewById(R.id.editTextTextPersonName);
+        etPassword = (EditText) findViewById(R.id.editTextTextPassword);
+        btnLogin = (Button) findViewById(R.id.authButton);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String login = etLogin.getText().toString();
+                String password = etPassword.getText().toString();
+
+                new LoginUser().execute(login, password);
+            }
+        });
+    }
+
+    public class LoginUser extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            String login = strings[0];
+            String password = strings[1];
+
+            OkHttpClient okHttpClient = new OkHttpClient();
+
+            RequestBody formBody = new FormBody.Builder()
+                    .add("login", login)
+                    .add("password", password)
+                    .build();
+            //);
+
+            Request request = new Request.Builder()
+                    .url(urlLogin)
+                    .post(formBody)
+                    .build();
+            Response response = null;
+            try {
+                response = okHttpClient.newCall(request).execute();
+                if (response.isSuccessful()) {
+                    String result = response.body().string();
+                    if (result.equalsIgnoreCase("login")) {
+                        Intent intent = new Intent(MainActivity.this, Dashboadr.class);
+                        startActivity(intent);
+                        finish();
+                    }else {
+                        Toast toast = Toast.makeText(getApplicationContext(), "Неверный логин или пароль!", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                }
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
